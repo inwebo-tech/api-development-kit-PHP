e3b3b88a963d2809e28782aee16512eac44d713b
inwebo-technologies <40398738+inwebo-technologies@users.noreply.github.com>
2023-08-22 10:29:51 +0200

    Add files via upload

---
+++ php/src/_loader.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set("soap.wsdl_cache_enabled", 0);
+
+//Loading of files
+include_once './resources/settings.php';
+include_once './resources/helpers.php';
+require_once './API/_api.php';
+include_once './resources/apiFunctions.php';
+
+//Initializing main objects
+$helpers = new helpers();
+$apiFunctions =    new apiFunctions($serviceId, 
+                                    $wsdlProvisioningPath, 
+                                    $wsdlAuthenticationPath, 
+                                    $certPath, 
+                                    $certPassphrase, 
+                                    $iwApiBaseUrl, 
+                                    $withErrorTrace,
+                                    $withRESTResultTrace);
+
+$errorOccured = '<p class="error">An error occured. '
+        . 'You may check your configuration and activate traces in the "settings.php" file.</p>';
